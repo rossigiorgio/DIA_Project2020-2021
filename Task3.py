739aec2607d4c59cd16c8dfd955742856dcf0149
Amanda Seger <amandaseger98@gmail.com>
2021-07-02 14:03:42 +0200

    Task 3, revenue function

--- Task3.py
+++ Task3.py
@@ -15,19 +15,23 @@ from Pricing.Learners.UCB1 import *
 #Fixed parameters: Bid
 #Known parameters: Daily clicks, daily cost per click
 
+    
+
 def main():
     T = 365
     n_experiments = 150
     price_env = PricingEnv()
     arms_candidates = np.array(price_env.prices)
     n_arms = len(arms_candidates)
-    prob = price_env.probabilities
+    conv_rate = price_env.probabilities
     classes = price_env.classes
+    costPerClick = ....
+    n = ...
     
     #HELP: How do I get the converstion rates here?
-    conv_rate = np.array([[1,2,3,4,5],
-                          [1,2,3,4,5],
-                          [1,2,3,4,5]])
+    #conv_rate = np.array([[1,2,3,4,5],
+                          #[1,2,3,4,5],
+                          #[1,2,3,4,5]])
     #Bid is fixed, 1 for every class?
     bid = np.array([[5],
                     [6], 
@@ -35,22 +39,48 @@ def main():
     
     ts_reward_per_experiment = []
     ucb_reward_per_experiment = []
-    
     #Learning simulation
     ts_learner = TS_Learner(n_arms = n_arms)
     ucb_learner = UCB1(n_arms = n_arms)
-    ts_reward_per_experiment = []
+    
+    ts_daily_reward= []
+    ucb_daily_reward= []
+    
     ucb_reward_per_experiment = []
     for t in range(0,T):
+        ts_daily_profits = 0
+        ucb_daily_profits = 0
+        #Revenue function
+        for c in range(0,3): #Classes
         #Thompson Sampling Learner
-        pulled_arm1 = ts_learner.pull_arm()
-        reward1 = price_env(pulled_arm1)
-        ts_learner.update(pulled_arm1, reward1)
+            ts_pulled_arm = ts_learner.pull_arm()
+            ts_reward = price_env(ts_pulled_arm)
+            ts_learner.update(ts_pulled_arm, ts_reward)
+            
+            #Revenue function
+            ts_class_profit = (t*conv_rate[ts_pulled_arm]*price-costPerClick)*n
+            
+            #UCB1 Learner
+            ucb_pulled_arm = ucb_learner.pull_arm()
+            ucb_reward = price_env(ucb_pulled_arm)
+            ucb_learner.update(ucb_pulled_arm, ucb_reward)
+            
+            #Revenue function
+            ucb_class_profit = (t*conv_rate[ucb_pulled_arm]*price-costPerClick)*n
+            
+            ts_daily_profits += ts_class_profit
+            ucb_daily_profits += usb_class_profit
+            
+        ts_daily_reward.append(ts_daily_profits)
+        ucb_daily_reward.append(ucb_daily_profits)
         
-        #UCB1 Learner
-        pulled_arm2 = ucb_learner.pull_arm()
-        reward2 = price_env(pulled_arm2)
-        ucb_learner.update(pulled_arm2, reward2)
-        
-    ts_reward_per_experiment.append(ts_reward_per_experiment)
-    ucb_reward_per_experiment.append(ucb_reward_per_experiment)
+    #ts_reward_per_experiment.append(ts_)
+    #ucb_reward_per_experiment.append(ucb_reward_per_experiment)
+    
+    
+
+            
+
+    
+
+    
